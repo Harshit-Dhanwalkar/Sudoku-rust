79cd46ea8d415dc40010ee21f8e1428070d5e0af
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2025-01-13 19:11:01 +0530

    Add binaries from cargo

---
+++ sudoku-solver/target/debug/deps/sudoku_solver-c1b0d9e907b065e2.d
@@ -0,0 +1,5 @@
+/home/harshitpd/Documents/GITHUB/Sudoku-rust/sudoku-solver/target/debug/deps/sudoku_solver-c1b0d9e907b065e2: src/main.rs
+
+/home/harshitpd/Documents/GITHUB/Sudoku-rust/sudoku-solver/target/debug/deps/sudoku_solver-c1b0d9e907b065e2.d: src/main.rs
+
+src/main.rs:
---
+++ sudoku-solver/target/debug/sudoku-solver.d
@@ -0,0 +1,1 @@
+/home/harshitpd/Documents/GITHUB/Sudoku-rust/sudoku-solver/target/debug/sudoku-solver: /home/harshitpd/Documents/GITHUB/Sudoku-rust/sudoku-solver/src/main.rs
